2605588310a81711931fc46b1f9fd84208e2342e
Enzo Gladiadis <30397502+Tr0j4n9@users.noreply.github.com>
2018-06-27 13:50:38 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,5 @@
 import sys
 import other
-
-def query_start_game(question, default="yes"):
-    valid = {"yes": True, "y": True, "ye": True,
-             "no": False, "n": False}
-    if default is None:
-        prompt = " [y/n] "
-    elif default == "yes":
-        prompt = " [Y/n] "
-    elif default == "no":
-        prompt = " [y/N] "
-    else:
-        raise ValueError("invalid default answer: '%s'" % default)
-
-    while True:
-        sys.stdout.write(question + prompt)
-        choice = raw_input().lower()
-        if default is not None and choice == '':
-            return valid[default]
-        elif choice in valid:
-            return valid[choice]
-        else:
-            sys.stdout.write("Please respond with 'yes' or 'no' "
-                             "(or 'y' or 'n').\n")
-            
             
             
 # Display intro
