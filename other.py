438953b9676c683bbb350dfadc0c8d1d9803ad30
Enzo Gladiadis <30397502+Tr0j4n9@users.noreply.github.com>
2018-06-27 13:51:38 +1200

    Update other.py

--- other.py
+++ other.py
@@ -9,7 +9,18 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
     
-    def query_start_game(question, default="yes"):
+import sys
+
+def query_yes_no(question, default="yes"):
+    """Ask a yes/no question via raw_input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+        It must be "yes" (the default), "no" or None (meaning
+        an answer is required of the user).
+
+    The "answer" return value is True for "yes" or False for "no".
+    """
     valid = {"yes": True, "y": True, "ye": True,
              "no": False, "n": False}
     if default is None:
@@ -30,4 +41,4 @@ class bcolors:
             return valid[choice]
         else:
             sys.stdout.write("Please respond with 'yes' or 'no' "
-"(or 'y' or 'n').\n")
+                             "(or 'y' or 'n').\n")
